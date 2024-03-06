1745b68533343ff7b7d9d11df2009edc62dacbaa
Angel <angelpombo.92@gmail.com>
2024-03-06 19:13:11 +0100

    aviso legal, politica de cookies... btn entradas anticipadas

---
+++ src/components/CondicionesGenerales/CondicionesGenerales.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+
+function CondicionesGenerales() {
+  return (
+    <section>
+      <h1>CONDICIONES GENERALES</h1>
+      <h2>CONDICIONES GENERALES DE ACCESO Y PARTICIPACIÓN EN EL FESTIVAL – GO! Electronic Festival</h2>
+      <p>Organizacion de eventos del norte SL (en adelante, la Compañía), manifiesta su respeto y cumplimiento de las disposiciones recogidas en la Ley Orgánica 15/1999, de 13 de Diciembre, de Protección de Datos de Carácter Personal. Por ello, le informa de su política de protección de datos para que Usted determine voluntariamente si desea facilitar sus datos personales a la Compañía.</p>
+      <div>
+        <h3>1. Introducción</h3>
+        <p>El Festival Go!Electronic Festival es un evento musical que se celebra en el entorno natural de Xardin Botanico/Aquapark Cerceda – Cerceda(A coruña,Galicia) organizado por ORGANIZACION DE EVENTOS DEL NORTE S.L VAT ESB56837719 C/alvite 86, sorrizo(Arteixo)cp15144, La coruña.</p>
+        <p>Los datos de contacto de la Organización son:</p>
+        <p>Correo Electrónico – goelectronicfest@hotmail.com</p>
+      </div>
+      <div>
+        <h3>2. Objeto</h3>
+        <p>Estas Condiciones Generales regulan el acceso y uso del recinto en el que se celebra el Festival Go!Electronic Festival por parte de todas aquellas personas que hayan adquirido una entrada a través de los cauces oficiales (en adelante, los usuarios).</p>
+        <p>Estas Condiciones Generales se complementan con las Condiciones Particulares que regulan determinados servicios ofrecidos por la Organización.</p>
+        <p>En caso de contradicción entre los términos recogidos en estas Condiciones Generales y en las Condiciones Particulares, éstas prevalecerán sobre aquéllas si bien tan sólo respecto de aquellos servicios sometidos a ellas.</p>
+      </div>
+      <div>
+        <h3>3. Aceptación</h3>
+        <p>La adquisición de una entrada al Festival, a través de los cauces habilitados por la Organización, implica el conocimiento y aceptación por los usuarios de las presentes Condiciones.</p>
+        <p>Los usuarios declaran disponer de la capacidad legal necesaria para aceptar estas Condiciones.</p>
+      </div>
+      <div>
+        <h3>4. Modificación</h3>
+        <p>En la medida en que la organización del Festival depende de diversos factores, ajenos a la Organización, estas Condiciones pueden ser modificadas unilateralmente por ella cuando lo considere oportuno o con la finalidad de adecuarse a los cambios logísticos o legislativos futuros.</p>
+        <p>Estas modificaciones serán notificadas convenientemente a los usuarios a través del correo electrónico o la página web www.goelectronicfestival.com, siendo válidas desde la fecha que se detalle en ellas. Si los usuarios no están de acuerdo con las citadas modificaciones, antes de que comience el Festival podrán solicitar la devolución del precio abonado por su entrada. Una vez iniciado, no tendrán esta posibilidad.</p>
+      </div>
+      <div>
+        <h3>5. Celebración del festival</h3>
+        <p>El festival Go!electronic Festival se celebrará en Xardin botanico/Aquapark Cerceda – Cerceda, la coruña  (Galicia) los días 13 y 14 de Septiembre de 2024 , conforme al cartel de artistas y horarios que figura en www.goelectronicfestival.com, reservándose la Organización la posibilidad de modificar los artistas y/u horarios que figuran en él, cuando concurran causas de fuerza mayor o cualquier otra ajena a su voluntad. En este supuesto, los usuarios no tendrán derecho a la devolución del precio de entrada salvo que la modificación sea esencial, entendiendo por tal, aquella que afecte, al menos, al 50% de los artistas.</p>
+        <p>En el caso de que la suspensión o modificación del cartel se produjese una vez iniciado el Festival, por razones de caso fortuito o fuerza mayor, los usuarios no tendrán derecho a la devolución de la entrada.</p>
+      </div>
+      <div>
+        <h3>6. Condición de Usuario. Adquisición de la entrada</h3>
+        <p>Tendrán la condición de usuarios del Festival todas las personas físicas, mayores de dieciocho (18) años, que adquieran una entrada, previa aceptación de las Condiciones Generales de Compra, a través de cualquiera de los siguientes canales:</p>
+        <ul>
+          <li>www.goelectronicfestival.com</li>
+          <li>www.shotgun.live</li>
+          <li>Taquillas instaladas en el recinto del festival</li>
+        </ul>
+        <p>La Organización no garantiza la validez de las entradas adquiridas a través de canales distintos de los anteriormente señalados, reservándose la posibilidad de no admitir en el recinto del Festival a aquellas personas que las porten.</p>
+        <span>Se prohibe expresamente la reventa de entradas.</span>
+        <p>En caso de pérdida o deterioro de las entradas, la Organización no asume ninguna responsabilidad y, en consecuencia, los usuarios no podrán acceder al recinto del Festival.</p>
+        <p>Una vez adquirida la entrada, ésta no será cambiada ni devuelto su importe, excepto en casos de cancelación del evento. Si por causa de fuerza mayor no pudiera empezar el Festival, la Organización podrá señalar nuevo día y hora para la celebración del mismo, siendo válida la entrada para la fecha definitiva. En caso de suspensión del evento, la Organización se compromete a devolver el importe de las entradas en un plazo máximo de 15 días a contar desde la fecha que conste en ellas.</p>
+        <p>La posesión de la entrada no da derecho a su poseedor o a terceros a utilizar la misma o su contenido con fines publicitarios, de marketing o de promoción (incluidos concursos, regalos y/o sorteos). Si lo hiciera, la Organización anulará la entrada, sin perjuicio de reclamar los daños y perjuicios que esta situación le hubiese ocasionado.</p>
+      </div>
+      <div>
+        <h3>7. Admisión</h3>
+        <p>En el recinto del Festival, el personal de la Organización verificará la validez de la entrada y hará entrega al usuario de una pulsera identificativa que le permitirá acceder al mismo. Si la entrada se encuentra en mal estado, rota o presenta indicios de falsificación, no se entregará la pulsera que da acceso.</p>
+        <p>Para acceder al recinto es imprescindible llevar la pulsera. La pulsera es personal e intransferible. En caso de pérdida o robo de la misma, los usuarios perderán la posibilidad de acceder al recinto.</p>
+        <p>El acceso al recinto puede resultar ocasionalmente lento, por lo que la Organización aconseja a los usuarios prudencia y paciencia. En algunos momentos es posible que se registren colas y ligeras retenciones, por lo que se recomienda a los usuarios que lleguen con puntualidad.</p>
+        <p>Las ZONAS DE DESCANSO del Festival estarán habilitados desde el Viernes 13 de Septiembre de 2024 a las 12:00  hasta el LUNES 15 de septiembre de 2024 a las 12:00</p>
+        <h4>NORMAS:</h4>
+        <ul>
+          <li>A. Queda prohibida la entrada a menores de 18 años. Es imprescindible para todas las personas llevar consigo el Documento Nacional de Identidad.</li>
+          <li>B. Se reserva el derecho de admisión de toda persona cuya actitud y presencia pueda ser considerada inadecuada, ofensiva, violenta o dé muestras de encontrarse bajo los efectos del alcohol y/o drogas.</li>
+          <li>C. Está prohibido introducir en el recinto del Festival drogas u otro tipo de substancias prohibidas por la ley, grabadoras de audio y vídeo, armas o instrumentos contundentes, cortantes, que sean peligrosos a criterio de la Organización o que estén prohibidos por la normativa vigente.</li>
+          <li>D. Queda expresamente prohibido realizar fotografías, grabaciones o videos de los espectáculos que forman parte del Festival, salvo que se cuente con la autorización expresa de la Organización.</li>
+          <li>E. Queda expresamente prohibido acceder a los escenarios o lugares de actuación de los artistas, salvo que esté previsto en el desarrollo del propio espectáculo.</li>
+          <li>F. Los usuarios deben comportarse en el Recinto del Festival actuando con absoluto respeto a los derechos de terceros. Deben seguir con atención los consejos y pautas de comportamiento que les traslade el personal de la Organización pues son por su propio interés y seguridad, así como la de todos los usuarios, con el objetivo de garantizar que el Festival se desarrolla sin ningún tipo de percance.</li>
+          <li>G. Desde que se accede al recinto del Festival queda absolutamente prohibida la introducción en el mismo de cualquier tipo de comida o bebida. Sólo está permitido introducir COMIDA EN LA ZONA DE DESCANSO.</li>
+        </ul>
+        <p>La Organización podrá denegar el acceso o expulsar del recinto a los usuarios que contravengan las normas recogidas en los párrafos anteriores o que generen situaciones que la Organización considere que pueden generar riesgos tanto para ellos como para otros usuarios, responsabilizándose personalmente de los daños y perjuicios que causen a terceros.</p>
+      </div>
+      <div>8. Imágenes obtenidas durante la celebración del festival
+        <p>Durante la celebración del Festival tanto la Organización como los medios de comunicación por ella habilitados podrán realizar videos y fotografías en los que aparezcan imágenes de los usuarios.</p>
+        <p>Los usuarios autorizan expresamente a la Organización para que ésta pueda utilizar su imagen en los citados videos y fotografías, que podrán ser empleados para confeccionar material (impreso o audiovisual) destinado a promover, difundir y dar a conocer el Festival Go!Electronic Festival  y cuyos derechos de explotación (reproducción, distribución, comunicación pública y transformación) corresponderán en exclusiva a la Organización.</p>
+        <p>Igualmente, autoriza a los medios de comunicación habilitados por la Organización para utilizar dicha imagen en la publicación de noticias y/o reportajes relacionados con la celebración del Festival.</p>
+        <p>Esta autorización se entenderá otorgada con carácter gratuito, para el ámbito territorial mundial y por el plazo máximo que fija la legislación aplicable.</p>
+      </div>
+      <div>
+        <h3>9. Protección de datos de carácter personal</h3>
+        <p>Todos los datos de los usuarios podrán ser comunicados a las Fuerzas y Cuerpos de Seguridad del Estado, a las Administraciones Públicas con competencias en materia de eventos y a Juzgados y Tribunales en el curso de inspecciones, procedimientos sancionadores y/o judiciales.</p>
+        <p>Al adquirir su entrada y aceptar estas Condiciones Generales, los usuarios consienten expresamente el tratamiento de sus datos personales en los términos antes expuestos.</p>
+        <p>Los usuarios que lo deseen pueden ejercitar gratuitamente los derechos de acceso, rectificación, cancelación y oposición dirigiéndose por escrito a ORGANIZACION DE EVENTOS DEL NORTE S.LVAT ESB56837719 C/alvite 86, sorrizo(Arteixo)cp15144 A coruña. o a goelectronicfest@hotmail.com, indicando la petición que formulan y adjuntando a su solicitud, una fotocopia de su DNI.</p>
+      </div>
+      <div>
+        <h3>10. Responsabilidad</h3>
+        <p>La Organización suscribirá el correspondiente seguro de Responsabilidad Civil obligatorio. No obstante lo anterior, la Organización no se responsabiliza de las pérdidas, robos, hurtos o cualquier daño que sufran las pertenencias de los usuarios durante la celebración del Festival. Los usuarios son los responsables de custodiarlas debidamente.</p>
+      </div>
+      <div>
+        <h3>11. Comunicaciones</h3>
+        <p>A efectos de las presentes Condiciones y para cualquier comunicación que sea precisa entre la Organización y los Usuarios, éstos podrán dirigirse a aquélla por medio de correo postal o electrónico enviado a las direcciones que figuran en el apartado 1 de estas Condiciones Generales.</p>
+        <p>Los usuarios aceptan expresamente la utilización del correo electrónico como procedimiento válido para la remisión de comunicaciones.</p>
+        <p>En el supuesto de que deseen presentar alguna reclamación, se les recuerda que tienen a su disposición las correspondientes Hojas de Reclamaciones.</p>
+      </div>
+      <div>
+        <h3>Plataforma de resolución de litigios en línea</h3>
+        <p>Si ha adquirido la entrada a nuestro evento online y en el eventual caso de que no haya sido posible resolver una incidencia dirigiéndose al correo electrónico Anderson@shotgun.live    de conformidad con lo establecido en el artículo 14.1 del Reglamento 524/2013, de 21 de mayo de 2013, sobre resolución de litigios en línea en materia de consumo, le informamos de que usted tiene la posibilidad de recurrir a una entidad de resolución alternativa de litigios en materia de consumo accesible a través de la dirección https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage</p>
+      </div>
+      <div>
+        <h3>13. Legislación aplicable</h3>
+        <p>Las presentes Condiciones Generales se rigen por la legislación española.</p>
+        <p>Para cuantas cuestiones pudieran suscitarse sobre la interpretación, aplicación, cumplimiento o incumplimiento de lo establecido en estas Condiciones Generales, los usuarios, con renuncia expresa a cualquier otro fuero que pudiera corresponderles, se someterán a la jurisdicción de los Juzgados y Tribunales de la coruña (galicia), lugar de cumplimiento de la obligación.</p>
+      </div>
+    </section>
+  )
+}
+
+export default CondicionesGenerales
